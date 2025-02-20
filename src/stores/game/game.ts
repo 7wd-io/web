0a488cb12bceadf21e87445ea73a8454a5d1e9b9
Danila Stivrinsh <ridzhi@livexp.com>
2025-02-20 12:22:28 +0200

    upd game store

--- src/stores/game/game.ts
+++ src/stores/game/game.ts
@@ -1,7 +1,8 @@
+import { api } from 'boot/axios';
+import { computed, ref } from 'vue';
 import { defineStore } from 'pinia';
 import {
   CardId,
-  City,
   Game,
   GameId,
   Phase,
@@ -10,123 +11,144 @@ import {
 } from 'src/models/game';
 import { Nickname } from 'src/models/account';
 import { useAccountStore } from 'stores/account';
-import { api } from 'boot/axios';
 import { useLogStore } from 'stores/game/log';
 import { usePlayAgainStore } from 'stores/game/playAgain';
 
-export const useGameStore = defineStore('game', {
-  state: () => ({
-    id: 0 as GameId,
-    state: {} as State,
-    finished: false as boolean,
-    lastMoveAt: '' as string,
-    left: '' as Nickname,
-    right: '' as Nickname,
-    players: {} as Record<
-      Nickname,
-      { rating: number; points: number; clock: number }
-    >,
-    chan: 0 as number,
-  }),
-  getters: {
-    city(state): (name: Nickname) => City {
-      return (name: Nickname) =>
-        name === state.state.me.name ? state.state.me : state.state.enemy;
-    },
-    enemy(state): (name: Nickname) => Nickname {
-      return (name) => (name === state.left ? state.right : state.left);
-    },
-    isMyTurn(state) {
-      const $account = useAccountStore();
+export const useGameStore = defineStore('game', () => {
+  const id = ref<GameId>(0);
+  const state = ref<State>({} as State);
+  const finished = ref(false);
+  const lastMoveAt = ref('');
+  const left = ref<Nickname>('');
+  const right = ref<Nickname>('');
+  const players = ref<
+    Record<Nickname, { rating: number; points: number; clock: number }>
+  >({});
+  const chan = ref(0);
 
-      return $account?.user.nickname === state.state?.me?.name;
-    },
-    isMember(state) {
-      const $account = useAccountStore();
+  const city = computed(
+    () => (name: Nickname) =>
+      name === state.value.me.name ? state.value.me : state.value.enemy
+  );
 
-      return (
-        $account?.user.nickname === state.left ||
-        $account?.user.nickname === state.right
-      );
-    },
-    isOver(state): boolean {
-      return state.state.phase === Phase.over;
-    },
-    isFinished(state): boolean {
-      return state.finished;
-    },
-    isPlayableCard(state): (id: CardId) => boolean {
-      return (id) =>
-        Object.prototype.hasOwnProperty.call(
-          state.state.cardItems.playable || {},
-          id
-        ) || false;
-    },
-  },
-  actions: {
-    async load(id: GameId) {
-      const $account = useAccountStore();
-      const $log = useLogStore();
-      const { data } = await api.get<Game>(`/game/${id}`);
+  const enemy = computed(
+    () => (name: Nickname) => name === left.value ? right.value : left.value
+  );
 
-      this.id = data.id;
-      const me = $account.user.nickname;
-      this.left =
-        me === data.host.name || me === data.guest.name ? me : data.host.name;
-      this.right = me === data.guest.name ? data.host.name : data.guest.name;
+  const isMyTurn = computed(() => {
+    const $account = useAccountStore();
 
-      this.players[data.host.name] = {
+    return $account?.user.nickname === state.value?.me?.name;
+  });
+
+  // @TODO Participant?
+  const isMember = computed(() => {
+    const $account = useAccountStore();
+
+    return (
+      $account?.user.nickname === left.value ||
+      $account?.user.nickname === right.value
+    );
+  });
+
+  const isOver = computed(() => state.value.phase === Phase.over);
+  // @TODO cut?
+  const isFinished = computed(() => finished.value);
+
+  const isPlayableCard = computed(
+    () => (id: CardId) =>
+      Object.prototype.hasOwnProperty.call(
+        state.value.cardItems.playable || {},
+        id
+      ) || false
+  );
+
+  async function load(v: GameId) {
+    const $account = useAccountStore();
+    const $log = useLogStore();
+    const { data } = await api.get<Game>(`/game/${v}`);
+
+    id.value = data.id;
+    const me = $account.user.nickname;
+    left.value =
+      me === data.host.name || me === data.guest.name ? me : data.host.name;
+    right.value = me === data.guest.name ? data.host.name : data.guest.name;
+
+    players.value = {
+      [data.host.name]: {
         rating: data.host.rating,
         points: data.host.points,
         clock: 0,
-      };
-
-      this.players[data.guest.name] = {
+      },
+      [data.guest.name]: {
         rating: data.guest.rating,
         points: data.guest.points,
         clock: 0,
-      };
+      },
+    };
 
-      if (data.clock) {
-        this.lastMoveAt = data.clock.lastMoveAt;
-        this.players[this.left].clock = data.clock.values[this.left];
-        this.players[this.right].clock = data.clock.values[this.right];
-      }
+    if (data.clock) {
+      lastMoveAt.value = data.clock.lastMoveAt;
+      players.value[left.value].clock = data.clock.values[left.value];
+      players.value[right.value].clock = data.clock.values[right.value];
+    }
 
-      this.state = data.state;
-      this.finished = data.finished;
-      $log.records = data.log;
-    },
+    state.value = data.state;
+    finished.value = data.finished;
+    $log.records = data.log;
+  }
 
-    async goto(index: number) {
-      const { data } = await api.get<{ state: State }>(
-        `/game/${this.id}/state/${index}`
-      );
-      this.state = data.state;
-    },
+  async function goto(index: number) {
+    const { data } = await api.get<{ state: State }>(
+      `/game/${id.value}/state/${index}`
+    );
+    state.value = data.state;
+  }
 
-    update(m: UpdateMessage) {
-      const $log = useLogStore();
-      const $playAgain = usePlayAgainStore();
+  function update(m: UpdateMessage) {
+    const $log = useLogStore();
+    const $playAgain = usePlayAgainStore();
 
-      this.state = m.state;
-      this.lastMoveAt = m.clock.lastMoveAt;
-      this.players[this.left].clock = m.clock.values[this.left];
-      this.players[this.right].clock = m.clock.values[this.right];
-      $log.records = [...$log.records, m.lastMove];
+    state.value = m.state;
+    lastMoveAt.value = m.clock.lastMoveAt;
+    players.value[left.value].clock = m.clock.values[left.value];
+    players.value[right.value].clock = m.clock.values[right.value];
+    $log.records = [...$log.records, m.lastMove];
 
-      if (this.isOver) {
-        $playAgain.show = this.isMember;
-        $playAgain.showBadges = true;
-      }
-    },
+    if (isOver.value) {
+      $playAgain.show = isMember.value;
+      $playAgain.showBadges = true;
+    }
+  }
 
-    tick() {
-      this.chan += 1;
-    },
+  function tick() {
+    chan.value += 1;
+  }
 
-    async createWithBot() {
-      return await api.post('/game');
-    },
-  },
+  async function createWithBot() {
+    return await api.post('/game');
+  }
+
+  return {
+    id,
+    state,
+    finished,
+    lastMoveAt,
+    left,
+    right,
+    players,
+    chan,
+    city,
+    enemy,
+    isMyTurn,
+    isMember,
+    isOver,
+    isFinished,
+    isPlayableCard,
+    load,
+    goto,
+    update,
+    tick,
+    createWithBot,
+  };
 });
