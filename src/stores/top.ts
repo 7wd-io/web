04efde507d0195dd64aafbe11bab4599a6924d5f
Danila Stivrinsh <ridzhi@livexp.com>
2025-02-21 14:53:22 +0200

    upd top store

--- src/stores/top.ts
+++ src/stores/top.ts
@@ -1,15 +1,18 @@
+import { ref } from 'vue';
 import { defineStore } from 'pinia';
 import { RatingTop } from 'src/models/game';
 import { api } from 'boot/axios';
 
-export const useTopStore = defineStore('top', {
-  state: () => ({
-    players: [] as RatingTop,
-  }),
-  actions: {
-    async load() {
-      const { data } = await api.get<{ players: RatingTop }>('/top');
-      this.players = data.players;
-    },
-  },
+export const useTopStore = defineStore('top', () => {
+  const players = ref([] as RatingTop);
+
+  async function load() {
+    const { data } = await api.get<{ players: RatingTop }>('/top');
+    players.value = data.players;
+  }
+
+  return {
+    players,
+    load,
+  };
 });
