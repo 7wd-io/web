726f3a328e78319b0282c8a09a77058e89a34b0f
Danila Stivrinsh <ridzhi@livexp.com>
2025-02-21 10:19:58 +0200

    upd online store

--- src/stores/online.ts
+++ src/stores/online.ts
@@ -1,3 +1,4 @@
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 import { Rating, RatingMap } from 'src/models/game';
 import { Nickname } from 'src/models/account';
@@ -6,29 +7,33 @@ import { useAccountStore } from 'stores/account';
 import { api } from 'boot/axios';
 import _toPairs from 'lodash-es/toPairs';
 
-export const useOnlineStore = defineStore('online', {
-  state: () => ({
-    players: {} as RatingMap,
-  }),
-  getters: {
-    desc(state): [Nickname, Rating][] {
-      return _toPairs(state.players).sort((a, b) => b[1] - a[1]);
-    },
-    count(state) {
-      return Object.keys(state.players).length;
-    },
-    search(state) {
-      const $account = useAccountStore();
+export const useOnlineStore = defineStore('online', () => {
+  const players = ref({} as RatingMap);
 
-      return Object.keys(state.players).filter(
-        (player) => player !== $account.user.nickname && player !== botNickname
-      );
-    },
-  },
-  actions: {
-    async load() {
-      const { data } = await api.get<{ data: RatingMap }>('/online');
-      this.players = data.data;
-    },
-  },
+  const desc = computed((): [Nickname, Rating][] => {
+    return _toPairs(players.value).sort((a, b) => b[1] - a[1]);
+  });
+
+  const count = computed(() => Object.keys(players.value).length);
+
+  const search = computed(() => {
+    const $account = useAccountStore();
+
+    return Object.keys(players.value).filter(
+      (player) => player !== $account.user.nickname && player !== botNickname
+    );
+  });
+
+  async function load() {
+    const { data } = await api.get<{ data: RatingMap }>('/online');
+    players.value = data.data;
+  }
+
+  return {
+    players,
+    desc,
+    count,
+    search,
+    load,
+  };
 });
