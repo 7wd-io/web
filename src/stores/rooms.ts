f6c09fc52058f753f750d49cce3be518005bfba3
Danila Stivrinsh <ridzhi@livexp.com>
2025-02-21 10:30:20 +0200

    upd rooms store

--- src/stores/rooms.ts
+++ src/stores/rooms.ts
@@ -1,89 +1,101 @@
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 import { Room, RoomOptions } from 'src/models/game';
 import { api } from 'boot/axios';
 import { useAccountStore } from 'stores/account';
 import { RoomId } from 'src/models/room';
 
-export const useRoomsStore = defineStore('rooms', {
-  state: () => ({
-    rooms: [] as Room[],
-  }),
-  getters: {
-    ordered(state) {
-      const $account = useAccountStore();
-      const head: Room[] = [];
-      const other: Room[] = [];
-      const tail: Room[] = [];
+export const useRoomsStore = defineStore('rooms', () => {
+  const rooms = ref([] as Room[]);
 
-      state.rooms.forEach((room) => {
-        if (
-          room.host === $account.user.nickname ||
-          room.guest === $account.user.nickname
-        ) {
-          head.unshift(room);
-        } else if (room.options.enemy === $account.user.nickname) {
-          head.push(room);
-        } else if (room.gameId) {
-          tail.push(room);
-        } else {
-          other.push(room);
-        }
-      });
+  const ordered = computed(() => {
+    const $account = useAccountStore();
+    const head: Room[] = [];
+    const other: Room[] = [];
+    const tail: Room[] = [];
 
-      return [...head, ...other, ...tail];
-    },
-  },
-  actions: {
-    async load() {
-      const { data } = await api.get<{ items: Room[] }>('/room');
-      this.rooms = data.items;
-    },
-
-    add(room: Room) {
-      if (!this.rooms.includes(room)) {
-        this.rooms.push(room);
+    rooms.value.forEach((room) => {
+      if (
+        room.host === $account.user.nickname ||
+        room.guest === $account.user.nickname
+      ) {
+        head.unshift(room);
+      } else if (room.options.enemy === $account.user.nickname) {
+        head.push(room);
+      } else if (room.gameId) {
+        tail.push(room);
+      } else {
+        other.push(room);
       }
-    },
+    });
 
-    update(room: Room) {
-      for (let i = 0; i < this.rooms.length; i += 1) {
-        if (this.rooms[i].id === room.id) {
-          this.rooms[i] = room;
-          break;
-        }
+    return [...head, ...other, ...tail];
+  });
+
+  async function load() {
+    const { data } = await api.get<{ items: Room[] }>('/room');
+    rooms.value = data.items;
+  }
+
+  async function add(r: Room) {
+    if (!rooms.value.includes(r)) {
+      rooms.value.push(r);
+    }
+  }
+
+  async function update(room: Room) {
+    for (let i = 0; i < rooms.value.length; i += 1) {
+      if (rooms.value[i].id === room.id) {
+        rooms.value[i] = room;
+        break;
       }
-    },
+    }
+  }
 
-    remove(id: string) {
-      const pos = this.rooms.findIndex((r) => r.id === id);
+  async function remove(id: string) {
+    const pos = rooms.value.findIndex((r) => r.id === id);
 
-      if (pos !== -1) {
-        this.rooms.splice(pos, 1);
-      }
-    },
+    if (pos !== -1) {
+      rooms.value.splice(pos, 1);
+    }
+  }
 
-    async create(o: RoomOptions) {
-      await api.post('/room', o);
-    },
+  async function create(o: RoomOptions) {
+    await api.post('/room', o);
+  }
 
-    async kick(id: RoomId) {
-      return await api.post(`/room/${id}/kick`);
-    },
+  async function kick(id: RoomId) {
+    return await api.post(`/room/${id}/kick`);
+  }
 
-    async cancel(id: RoomId) {
-      return await api.delete(`/room/${id}`);
-    },
+  async function cancel(id: RoomId) {
+    return await api.delete(`/room/${id}`);
+  }
 
-    async join(id: RoomId) {
-      return await api.post(`/room/${id}/join`);
-    },
+  async function join(id: RoomId) {
+    return await api.post(`/room/${id}/join`);
+  }
 
-    async leave(id: RoomId) {
-      return await api.post(`/room/${id}/leave`);
-    },
+  async function leave(id: RoomId) {
+    return await api.post(`/room/${id}/leave`);
+  }
 
-    async start(id: RoomId) {
-      return await api.post(`/room/${id}/start`);
-    },
-  },
+  async function start(id: RoomId) {
+    return await api.post(`/room/${id}/start`);
+  }
+
+  return {
+    rooms,
+    ordered,
+    load,
+    add,
+    update,
+    remove,
+    create,
+    kick,
+    cancel,
+    join,
+    leave,
+    start,
+  };
 });
