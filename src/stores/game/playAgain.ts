9276dfe47309df23e5b5e7f176c8d535bcd933ab
Danila Stivrinsh <ridzhi@livexp.com>
2025-02-21 09:55:18 +0200

    upd playAgain store

--- src/stores/game/playAgain.ts
+++ src/stores/game/playAgain.ts
@@ -1,20 +1,25 @@
+import { ref } from 'vue';
 import { defineStore } from 'pinia';
 import { Nickname } from 'src/models/account';
 import { GameId } from 'src/models/game';
 import { api } from 'boot/axios';
 
-export const usePlayAgainStore = defineStore('game:playAgain', {
-  state: () => ({
-    answers: {} as Record<Nickname, boolean>,
-    show: false,
-    showBadges: false,
-  }),
-  actions: {
-    async answer(gameId: GameId, answer: boolean) {
-      await api.post('/play-again', {
-        gameId,
-        answer,
-      });
-    },
-  },
+export const usePlayAgainStore = defineStore('game:playAgain', () => {
+  const answers = ref({} as Record<Nickname, boolean>);
+  const show = ref(false);
+  const showBadges = ref(false);
+
+  async function answer(gameId: GameId, answer: boolean) {
+    await api.post('/play-again', {
+      gameId,
+      answer,
+    });
+  }
+
+  return {
+    answers,
+    show,
+    showBadges,
+    answer,
+  };
 });
