0e1d9f6261aa6901e4597539f9d5850021ca0c72
Danila Stivrinsh <danilaridzhi@gmail.com>
2025-03-04 23:34:01 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 npm run build && \
 tar -cz -C dist/spa -f dist/web.tar . && \
-ssh ds rm -f /root/projects/7wd.io/web.tar && \
-ssh ds rm -fr /root/projects/7wd.io/web && \
-ssh ds mkdir /root/projects/7wd.io/web && \
-scp dist/web.tar ds:/root/projects/7wd.io && \
-ssh ds tar -xvf /root/projects/7wd.io/web.tar -C /root/projects/7wd.io/web
+ssh dev rm -f /root/app/7wd.io/web.tar && \
+ssh dev rm -fr /root/app/7wd.io/web && \
+ssh dev mkdir /root/app/7wd.io/web && \
+scp dist/web.tar ds:/root/app/7wd.io && \
+ssh dev tar -xvf /root/app/7wd.io/web.tar -C /root/app/7wd.io/web
